1fcfb4a414e5430fce7da109e96271a6b5f41a10
Richard Šmajda <Riso8500@gmail.com>
2019-04-06 13:39:14 +0200

    Added StyleProviders

--- PISFrontend/screens/HomeScreen.js
+++ PISFrontend/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import getTheme from "../native-base-theme/components";
 import material from "../native-base-theme/variables/material";
-import { Container, Content, Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
+import { Container, Content, Header, Left, Body, Right, Button, Icon, Title, StyleProvider } from 'native-base';
 
 
 export default class HomeScreen extends Component {
@@ -10,6 +10,7 @@ export default class HomeScreen extends Component {
     const { navigation } = this.props;
     const name = navigation.getParam('name', '');
     return (
+    <StyleProvider style={getTheme(material)}>
       <Container>
             <Header style={{ backgroundColor: '#00e6b8'}} androidStatusBarColor="#00e6b8">
               <Body>
@@ -22,6 +23,7 @@ export default class HomeScreen extends Component {
               </Right>
             </Header>
       </Container>
+      </StyleProvider>
     );
   }
 }
