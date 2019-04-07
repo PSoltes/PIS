2648c54d3db46bd535e6fef7fb723ebfdac3b3a8
Pavol Šoltés <soltes.pavol@gmail.com>
2019-04-07 15:43:11 +0200

    formatting fix

--- PISFrontend/scripts/Update.js
+++ PISFrontend/scripts/Update.js
@@ -1,28 +1,27 @@
 import SoapRequest from "react-native-soap-request";
 
-export default async function Update(object,id, entity)
+export default async function Update(object, id, entity)
 {
   return new Promise( async function(resolve, reject){
 
     const soapRequest = new SoapRequest({
-      requestURL: "http://labss2.fiit.stuba.sk/pis/ws/Students/Team035"+entity
+      requestURL: "http://labss2.fiit.stuba.sk/pis/ws/Students/Team035" + entity
     });
 
     const xmlRequest = soapRequest.createRequest({
       "typ:update": {
         attributes: {
           "xmlns:typ":
-          "http://labss2.fiit.stuba.sk/pis/students/team035"+ entity +"/types"
+          "http://labss2.fiit.stuba.sk/pis/students/team035" + entity + "/types"
         },
         'team_id': '035',
         'team_password':'zvbTTu',
         'entity_id': id,
-        entity : object
+        'entity' : object
       }
     });
     const response = await soapRequest.sendRequest();
 
-    //const result = response['SOAP-ENV:Envelope']['SOAP-ENV:Body']['0']['ns1:getByAttributeValueResponse']['0'][entity + 's']['0'][entity]['0'];
     resolve(response);
 
   });
