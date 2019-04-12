709aa8034e0b8dd6b729b89eb4514c1b3ed23d00
miro12325 <borecky1997@gmail.com>
2019-04-12 17:57:37 +0200

    dorobený loading a recenzie a všetko

--- PISFrontend/screens/AdminConfirmationScreen.js
+++ PISFrontend/screens/AdminConfirmationScreen.js
@@ -1,192 +1,185 @@
 import React, { Component } from "react";
 import getTheme from "../native-base-theme/components";
 import material from "../native-base-theme/variables/material";
-import { Container, Content, Header, Left,CheckBox, Body, Right, Button,Icon, Title,Text,Card,CardItem,H1,H3,H2} from 'native-base';
-import {View,Alert} from 'react-native';
+import { Container, Content, Header, Left, CheckBox, Body, Right, Button, Icon, Title, Text, Card, CardItem, H1, H3, H2 } from 'native-base';
+import { View, Alert, ActivityIndicator } from 'react-native';
 import Update from '../scripts/Update';
 import Delete from '../scripts/Delete';
-let item,col;
+let item, col;
 export default class AdminConfirmationScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      check: false
+    };
+  }
 
-  async Accept(){
-    console.warn("ideme");
-   let retrn;
-   item.conf.approved_at=new Date().toISOString;
-    await Update(item.conf, item.conf.id, 'comment').then(
-      function(respons) {
-        console.warn(respons);
-        retrn=respons;
+  async Accept() {
+    let retrn;
+    item.conf.spoiler_flag=this.state.check;
+    item.conf.approved_at =(new Date()).toISOString();
+    await Update(item.conf, item.conf.id, item.conf.name).then(
+      function (respons) {
+        retrn = respons;
       },
-      function(err) {
+      function (err) {
         console.log(err);
       }
-      );
-      console.warn("ideme overiť");
-      if(retrn){
-        Alert.alert(
-          'Upozornenie',
-          'Úspešné schválenie.',
-          [
-            {text: 'OK',onPress: () =>{
+    );
+      Alert.alert(
+        'Upozornenie',
+        'Príspevok bol úspešne schválené.',
+        [
+          {
+            text: 'OK', onPress: () => {
               this.props.navigation.navigate('AdminListScreen');
-              }}
-          ],
-          {cancelable: false},
-          );
-        console.warn('Pridané');
-      }
-      else{
-        Alert.alert(
-          'Upozornenie',
-          'Došlo k chybe pri ukladaní dát.',
-          [
-            {text: 'OK'}
-          ],
-          {cancelable: false},
-          );
-          console.warn('Nepridané');
-      }
-
+            }
+          }
+        ],
+        { cancelable: false },
+      );
+    this.setState({ loading: false });
   }
-  async Decline(){
+  async Decline() {
+    this.setState({ loading: true });
     let retrn;
     await Delete(item.conf.id, 'comment').then(
-      function(respons) {
-        console.warn(respons);
-        retrn=respons;
+      function (respons) {
+        retrn = respons;
       },
-      function(err) {
+      function (err) {
         console.log(err);
       }
-      );
-      if(retrn){
-        Alert.alert(
-          'Upozornenie',
-          'Úspešné zamietnuté.',
-          [
-            {text: 'OK',onPress: () =>{
+    );
+      Alert.alert(
+        'Upozornenie',
+        'Príspevok úspešne zamietnutý.',
+        [
+          {
+            text: 'OK', onPress: () => {
               this.props.navigation.navigate('AdminListScreen');
-              }}
-          ],
-          {cancelable: false},
-          );
-        console.warn('Pridané');
-      }
-      else{
-        Alert.alert(
-          'Upozornenie',
-          'Došlo k chybe pri ukladaní dát.',
-          [
-            {text: 'OK'}
-          ],
-          {cancelable: false},
-          );
-          console.warn('Nepridané');
-      }
+            }
+          }
+        ],
+        { cancelable: false },
+      );
+    this.setState({ loading: false });
   }
   render() {
-   
+
     const { navigation } = this.props;
-     item = navigation.getParam('items', {});
-     let expresion='';
-     if(item.conf.has_wrong_expression){
-      expresion='NEPREŠIEL'
-        col='red';
-     }
-     else {
-       expresion='PREŠIEL'
-        col='green';
+    item = navigation.getParam('items', {});
+    let expresion = '';
+    if (item.conf.has_wrong_expression) {
+      expresion = 'NEPREŠIEL'
+      col = 'red';
+    }
+    else {
+      expresion = 'PREŠIEL'
+      col = 'green';
 
-     }
-
+    }
+    let button;
+    if (this.state.loading) {
+      button = <ActivityIndicator size="small" color="#0FDDAF" />;
+    } else {
+      button = <View></View>
+    }
     return (
-    <Container>
-      <Header style={{ backgroundColor: '#00e6b8'}} androidStatusBarColor="#00e6b8">
-        <Body>
-        <Title>Potvrdenie</Title>
-        </Body>
-        <Right>
-          <Button transparent
-          onPress={() => {
-            this.props.navigation.navigate('AdminListScreen');
-          }}
-          >
-            <Icon name="close" />
-          </Button>
-        </Right>
-      </Header>
-      <Content width="90%" style={{flex:1,alignSelf:'center'}}>
-        <View style={{flex:1,borderBottomWidth:3,borderColor:'#00e6b8',alignItems:'flex-start',margin:10}}>
-          <View>
-            <H1 style={{color:'#00e6b8',marginBottom:10}}>{item.book.name}</H1>
+      <Container>
+        <Header style={{ backgroundColor: '#00e6b8' }} androidStatusBarColor="#00e6b8">
+          <Body>
+            <Title>Potvrdenie</Title>
+          </Body>
+          <Right>
+            <Button transparent
+              onPress={() => {
+                this.props.navigation.navigate('AdminListScreen');
+              }}
+            >
+              <Icon name="close" />
+            </Button>
+          </Right>
+        </Header>
+        <Content width="90%" style={{ flex: 1, alignSelf: 'center' }}>
+          <View style={{ flex: 1, borderBottomWidth: 3, borderColor: '#00e6b8', alignItems: 'flex-start', margin: 10 }}>
+            <View>
+              <H1 style={{ color: '#00e6b8', marginBottom: 10 }}>{item.book.name}</H1>
+            </View>
+            <View>
+              <H3 style={{ marginBottom: 10 }}>
+                {item.book.author}
+              </H3>
+            </View>
+          </View>
+          <View style={{ alignItems: 'flex-start', margin: 10 }}>
+            <View >
+              <Text style={stylos.bold}>Autor:<Text style={stylos.normal}>{item.user.name} {item.user.surname}</Text> </Text>
+            </View>
+            <View>
+              <Text style={stylos.bold}>Datum pridania: <Text style={stylos.normal}>{item.conf.created_at}</Text></Text>
+            </View>
+            <View>
+              <Text style={stylos.bold}>Kontrola zakázaných výrazov: <Text style={{ color: col }}>{expresion}</Text></Text>
+            </View>
           </View>
           <View>
-            <H3 style={{marginBottom:10}}>
-              {item.book.author}
-            </H3>
-          </View>
-        </View>
-        <View style={{alignItems:'flex-start',margin:10}}>
-          <View >
-            <Text style={stylos.bold}>Autor:<Text style={stylos.normal}>{item.user.name} {item.user.surname}</Text> </Text>
-          </View>
-          <View>
-            <Text style={stylos.bold}>Datum pridania: <Text style={stylos.normal}>{item.conf.created_at}</Text></Text>
-          </View>
-          <View>
-            <Text style={stylos.bold}>Kontrola zakázaných výrazov: <Text style={{color: col}}>{expresion}</Text></Text>
-          </View>
-        </View>
-        <View>
-          <Card style={{backGroundColor:'grey',font:10,flex:1,margin:10}}>
-            <Text style={{padding:5}}>
-              {item.conf.text}
+            <Card style={{ backGroundColor: 'grey', font: 10, flex: 1, margin: 10 }}>
+              <Text style={{ padding: 5 }}>
+                {item.conf.text}
               </Text>
-          </Card>
-        </View>
-        <View width="92%" style={{flexDirection:'row',flex:1,alignSelf: 'stretch',margin:10}}>
-          <Left>
-            <Text >Obsahuje spojler : </Text>
-          </Left>
-          <Right>
-            <CheckBox />
-          </Right>
-        </View>
-        <View width="100%" style={{flexDirection:'row',flex:1,alignSelf: 'stretch',margin:10}}>
-          <Left>
-            <Button
-            block
-            width="90%"
-            success
-            onPress={() => {
-              this.Accept();
-            }}
-            >
-              <Text> Schváliť</Text>
-            </Button>
-          </Left>
-          <Right>
-            <Button
-            block
-            width="90%"
-            danger
-            onPress={() => {this.Decline();
-            }}
-            >
-              <Text> Zamietnúť </Text>
-            </Button>
-          </Right>
-        </View>
-      </Content>
-    </Container>
+            </Card>
+          </View>
+          <View width="92%" style={{ flexDirection: 'row', flex: 1, alignSelf: 'stretch', margin: 10 }}>
+            <Left>
+              <Text >Obsahuje spojler : </Text>
+            </Left>
+            <Right>
+              <CheckBox checked={this.state.check} onPress={()=>{
+                this.setState({check:!this.state.check});
+              }}/>
+            </Right>
+          </View>
+          <View width="100%" style={{ flexDirection: 'row', flex: 1, alignSelf: 'stretch', margin: 10 }}>
+            <Left>
+              <Button
+                block
+                width="90%"
+                success
+                onPress={() => {
+                  this.setState({ loading: true });
+                  this.Accept();
+                  this.setState({ loading: false });
+                }}
+              >
+                <Text> Schváliť</Text>
+              </Button>
+            </Left>
+            <Right>
+              <Button
+                block
+                width="90%"
+                danger
+                onPress={() => {
+                  this.Decline();
+                }}
+              >
+                <Text> Zamietnúť </Text>
+              </Button>
+            </Right>
+          </View>
+          {button}
+        </Content>
+      </Container>
     );
   }
 }
-const stylos={
-  bold:{
+const stylos = {
+  bold: {
     fontWeight: 'bold',
   },
-  normal:{
+  normal: {
     fontWeight: 'normal',
   }
 }
--- PISFrontend/screens/AdminListScreen.js
+++ PISFrontend/screens/AdminListScreen.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import getTheme from "../native-base-theme/components";
 import material from "../native-base-theme/variables/material";
 import { Container, Content, Header, Left, Body, Right, Button, Icon, Title,Text,Card,CardItem,H3,H1,H2} from 'native-base';
-import {FlatList,View} from 'react-native';
+import {FlatList,View,ActivityIndicator} from 'react-native';
 import getByAttribute from "../scripts/GetByAttribute";
 import getById from '../scripts/GetById';
 import Dearray from '../scripts/Dearray';
@@ -12,26 +12,38 @@ export default class AdminListScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-     confirms: ''
+     confirms: '',
+     loading: false
+
     };
   }
   async LoadComments(){
-    let confarr= [];
+    this.setState({loading:true});
+    let comarr= [];let revarr=[];
     await getByAttribute('approved_at', null, 'comment').then(
     function(conf) {
       console.log(conf);
-      confarr=conf;
+      comarr=conf;
     },
     function(err) {
       console.log(err);
     }
     );
+    await getByAttribute('approved_at', null, 'review').then(
+      function(conf) {
+        console.log(conf);
+        revarr=conf;
+      },
+      function(err) {
+        console.log(err);
+      }
+      );
+      comarr.push(...revarr);
    let finarr=[];
-  if(confarr){
-    for(let obj in confarr){
+  if(comarr){
+    for(let obj in comarr){
       let usr=''; let book='';
-      console.log(confarr[obj].user_id[0]+" - "+confarr[obj].book_id[0]);
-      let bkid=confarr[obj].book_id[0];let usrid=confarr[obj].user_id[0];
+      let bkid=comarr[obj].book_id[0];let usrid=comarr[obj].user_id[0];
       await getById(usrid,'user').then(
       function(user) {;
         usr=Dearray(user);
@@ -49,18 +61,62 @@ export default class AdminListScreen extends Component {
         console.log(err);
       }
       );
-      let object = {key:confarr[obj].id[0],book:book,conf:Dearray(confarr[obj]),type:'Komentár',user:usr};
+      let object;
+      if(comarr[obj].name[0]=="review"){
+         object = {key:comarr[obj].id[0],book:book,conf:Dearray(comarr[obj]),type:'Recenzia',user:usr};
+      }
+      else{
+       object = {key:comarr[obj].id[0],book:book,conf:Dearray(comarr[obj]),type:'Komentár',user:usr};
+      }
       finarr.push(object);
     }
     this.setState({confirms:finarr});
     console.log(this.state.confirms);
   }
-
+  this.setState({loading:false});
   }
   async componentWillMount(){
     this.LoadComments();
   }
   render() {
+    let button;
+    if (this.state.loading) {
+      button =<ActivityIndicator size="large" color="#0FDDAF" />;
+              
+    } else {
+     button= <FlatList
+     data={this.state.confirms}
+     renderItem={({item})=>(
+       <Card style={{width:'90%',alignSelf: 'center'}}>
+         <CardItem width="100%" button onPress={()=>{
+           this.props.navigation.navigate('AdminConfirmationScreen',{items:item});
+          }}>
+           <Body width="100%">
+           <View width="100%">
+               <View>
+                 <H3 style={{color:'#00e6b8'}}>{item.book.name}</H3>
+               </View>
+               <View  width="100%" style={{font:15,flex:1,flexDirection:'row',alignSelf: 'stretch'}}>
+                 <View>
+                 <Text>
+                   {item.book.author}
+                 </Text>
+                   <Text>
+                     {item.user.name} {item.user.surname}
+                   </Text>
+                 </View>
+                 <Right>
+                       <Text>{item.type} </Text>
+                       <Text>{item.conf.created_at}</Text>
+                 </Right>
+               </View>
+           </View>
+           </Body>
+         </CardItem>
+       </Card>
+     )}
+     />
+    }
     return (
     <Container>
       <Header style={{ backgroundColor: '#00e6b8'}} androidStatusBarColor="#00e6b8">
@@ -80,44 +136,9 @@ export default class AdminListScreen extends Component {
           </Button>
         </Right>
       </Header>
-      <Content>
-      <FlatList
-      data={this.state.confirms}
-        /*[{key: '1',book:'Horiaca ríša',author:'Juraj Červenák',date:'12/03/2019',type:'Komentár',user:'Pavol Šoltés' },
-        {key: '2',book:'Horiaca ríša',author:'Juraj Červenák',date:'12/03/2019',type:'Komentár',user:'Pavol Šoltés' },
-        {key: '3',book:'Horiaca ríša',author:'Juraj Červenák',date:'12/03/2019',type:'Komentár',user:'Pavol Šoltés' },
-        {key: '4',book:'Horiaca ríša',author:'Juraj Červenák',date:'12/03/2019',type:'Komentár',user:'Pavol Šoltés' },
-        {key: '5',book:'Horiaca ríša',author:'Juraj Červenák',date:'12/03/2019',type:'Komentár',user:'Pavol Šoltés' }]}*/
-      renderItem={({item})=>(
-        <Card style={{width:'90%',alignSelf: 'center'}}>
-          <CardItem width="100%" button onPress={()=>{
-            this.props.navigation.navigate('AdminConfirmationScreen',{items:item});
-           }}>
-            <Body width="100%">
-            <View width="100%">
-                <View>
-                  <H3 style={{color:'#00e6b8'}}>{item.book.name}</H3>
-                </View>
-                <View  width="100%" style={{font:15,flex:1,flexDirection:'row',alignSelf: 'stretch'}}>
-                  <View>
-                  <Text>
-                    {item.book.author}
-                  </Text>
-                    <Text>
-                      {item.user.name} {item.user.surname}
-                    </Text>
-                  </View>
-                  <Right>
-                        <Text>{item.type} </Text>
-                        <Text>{item.conf.created_at}</Text>
-                  </Right>
-                </View>
-            </View>
-            </Body>
-          </CardItem>
-        </Card>
-      )}
-      /></Content>
+      <Content contentContainerStyle={{flex:1,flexDirection:'column',justifyContent:'center'}}>
+        {button}
+      </Content>
     </Container>
     );
   }
